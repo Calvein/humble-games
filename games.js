23a4b03451f8db50558182e341de1431c079e6fd
muntdefems <muntdefems@yahoo.es>
2013-06-08 11:37:29 +0300

    Drifter now available for Linux
    Sources:
    https://twitter.com/celsiusgs/status/343157665062604800
    http://celsiusgs.com/drifter/preorder.php

--- games.js
+++ games.js
@@ -932,7 +932,7 @@ var games = [
         "platform": {
             "android": false,
             "windows": true,
-            "linux": false,
+            "linux": true,
             "mac": true,
             "audio": false
         },
