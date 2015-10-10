7f8109c75c00267178bbc40b92b90b0218eacbd6
muntdefems <muntdefems@yahoo.es>
2015-10-10 21:36:02 +0200

    Updated "Where is my Heart"

--- scripts/games.js
+++ scripts/games.js
@@ -35622,7 +35622,7 @@ var games = [
             "audio": false
         },
         "drm": {
-            "steam": false,
+            "steam": true,
             "desura": false,
             "gog": false,
             "drm-free": true
